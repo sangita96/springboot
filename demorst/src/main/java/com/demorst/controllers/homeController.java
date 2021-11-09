1c750dfa5fca8471ed3c8d369b3369d62e036baa
sangita96 <sangita.jana1606@gmail.com>
2021-11-09 20:23:02 +0530

    get version

--- demorst/src/main/java/com/demorst/controllers/homeController.java
+++ demorst/src/main/java/com/demorst/controllers/homeController.java
@@ -1,14 +1,35 @@
 package com.demorst.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.demorst.dao.AlienrepoI;
+import com.demorst.models.Alien;
 
 @Controller
 public class homeController {
 	@Autowired
-	AlienrepoI repo;//testing 2nd
+	AlienrepoI repo;
+	
+	@RequestMapping("/aliens")
+	@ResponseBody
+	public List<Alien> getAliens()
+	{
+		return repo.findAll();
+	}
+	
+	@RequestMapping("/aliens/{aid}")
+	@ResponseBody
+	public Optional<Alien> getAliensById(@PathVariable("aid") int aid)
+	{
+		return repo.findById(aid);
+	}
 	
 
 }
