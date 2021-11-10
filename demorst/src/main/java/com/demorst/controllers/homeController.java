087a1b38c04d5cf05da2651d1385a1c682e13011
sangita96 <sangita.jana1606@gmail.com>
2021-11-10 15:44:23 +0530

    get put post delete using controller

--- demorst/src/main/java/com/demorst/controllers/homeController.java
+++ demorst/src/main/java/com/demorst/controllers/homeController.java
@@ -5,31 +5,53 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.demorst.dao.AlienrepoI;
 import com.demorst.models.Alien;
 
-@Controller
+@RestController
 public class homeController {
 	@Autowired
 	AlienrepoI repo;
 	
-	@RequestMapping("/aliens")
-	@ResponseBody
+	@PostMapping("/aliens")
+	public Alien addAlien(Alien alien)
+	{
+	    repo.save(alien);
+		return alien;
+	}
+	
+	@GetMapping("/aliens")
 	public List<Alien> getAliens()
 	{
 		return repo.findAll();
 	}
 	
-	@RequestMapping("/aliens/{aid}")
-	@ResponseBody
+	@GetMapping("/aliens/{aid}")
 	public Optional<Alien> getAliensById(@PathVariable("aid") int aid)
 	{
 		return repo.findById(aid);
 	}
 	
+	@DeleteMapping("/aliens/{aid}")
+	public List<Alien> deleteAlien(@PathVariable int aid)
+	{
+	    repo.deleteById(aid);
+		return repo.findAll();
+	}
 
+	@PutMapping("/aliens")
+	public List<Alien> saveOrUpdateAlien(Alien alien)
+	{
+		repo.save(alien);;
+		return repo.findAll();
+	}
 }
